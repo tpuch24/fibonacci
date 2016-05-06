14277105264ccde7086235d0d0e4fc6753fa40e9
BeYouss <youssef.boubekri@gmail.com>
2016-05-06 07:10:27 -0400

    mode recursif

--- src/DisplayFiboIter.java
+++ src/DisplayFiboIter.java
@@ -11,24 +11,35 @@ public class DisplayFiboIter {
 	/**
 	 * Iterative mode
 	 * 
-	 * @param nbIter 
+	 * @param nbIter
 	 * @return
 	 */
-	
-	public static String displayNumberIter(int nbIter){
-		int valPrec1=1;
-		int valPrec2=0;
+
+	public static String displayNumberIter(int nbIter) {
+		int valPrec1 = 1;
+		int valPrec2 = 0;
 		int value;
-		String output="";
-		
-		for(int ind=0; ind<nbIter; ind++)
-		{		
-			value=valPrec1+valPrec2;
-			output += " "+value;
-			valPrec1=valPrec2;
-			valPrec2=value;
+		String output = "";
+
+		for (int ind = 0; ind < nbIter; ind++) {
+			value = valPrec1 + valPrec2;
+			output += " " + value;
+			valPrec1 = valPrec2;
+			valPrec2 = value;
 		}
-		
+
+		return output;
+	}
+
+	public static String displayNumberRecurs(int nbre, int valprec1, int valprec2) {
+		int value=0;
+		String output="";
+		if(nbre>0){
+			value=valprec1+valprec2;
+			valprec1=valprec2;
+			valprec2=value;
+			output+=" " +value+displayNumberRecurs(--nbre, valprec1, valprec2);
+					}
 		return output;
 	}
 }
